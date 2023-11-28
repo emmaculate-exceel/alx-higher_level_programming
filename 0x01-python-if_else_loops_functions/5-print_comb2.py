c64d8eb2e818f39e103bae6703f6f5050383be10
emmaculate-exceel <emmaculate2exceel@live.com>
2023-11-28 12:25:22 -0800

    print Num

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 99:
-        print("{}".format(str(i).zfill(2)), end=', ')
-    else:
-        print("{}".format(str(i).zfill(2)), end='')
+for num in range(0, 99):
+    print('{:02d}, '.format(num), end='')
+print('99')
