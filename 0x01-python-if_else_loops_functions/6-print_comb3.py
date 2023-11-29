f9184d3544011376b228d7645ca13c2321e1ca78
emmaculate-exceel <emmaculate2exceel@live.com>
2023-11-29 04:46:23 -0800

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for j in range(1 + i, 9):
+    for j in range(1 + i, 10):
         if not(i == 8 and j == 9): 
             print("{}{}, ".format(i, j), end="")
         else:
