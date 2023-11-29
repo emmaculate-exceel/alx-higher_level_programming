6c6e3eb1a6d62196312f690692c1ce33a0359b6b
emmaculate-exceel <emmaculate2exceel@live.com>
2023-11-29 07:18:10 -0800

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if islower(c):
+    if ord(c) >= 99 and ord(c) <= 122:
         return True
     else:
         return False
-islower = __import__('7-islower').islower
