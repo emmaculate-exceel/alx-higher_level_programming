96fa86997d3dc09f453e53edef30eb83b2e3620c
emmaculate-exceel <emmaculate2exceel@live.com>
2023-12-04 10:23:52 -0800

    index element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,6 @@
 def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None  # Return None if the index is out of range
 
-
-    for i, j in enumerate(
+    element = my_list[idx]  # Get the element at the specified index
+    return element  # Return the element found
