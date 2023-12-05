1729897a81a16ef331e165c264c414fc58e83a28
emmaculate-exceel <emmaculate2exceel@live.com>
2023-12-05 02:52:37 -0800

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        return ("None")  # Return None if the index is out of range
-
-    element = my_list[idx]  # Get the element at the specified index
-    return element  # Return the element foundi
+        return None  # Return None if the index is out of range
+    else:
+        element = my_list[idx]  # Get the element at the specified index
+        return element  # Return the element foundi
