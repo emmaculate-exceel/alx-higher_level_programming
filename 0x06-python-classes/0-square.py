6bfd27e005a17d7bc7cfd05969b40a350f9d421a
emmaculate-exceel <emmaculate2exceel@live.com>
2023-12-19 06:37:37 -0800

    square!

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, square):
-        self.square = square
+    def __init__(self, width, breath):
+        self.width = width
+        self.breath = breath
