0c500438714288c5a334fa784405cd91ea32d284
emmaculate-exceel <emmaculate2exceel@live.com>
2023-12-19 06:19:51 -0800

    square!

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    Square.right = ""
-    Square.left = ""
-    Square.up = ""
-    Square.down = ""
+    def __init__(self, square)
+        self.square = square
