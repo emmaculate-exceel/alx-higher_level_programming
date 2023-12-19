eab1c034ccdd82635af37240ed9444dc859d13ed
emmaculate-exceel <emmaculate2exceel@live.com>
2023-12-19 14:25:11 -0800

    3sqr

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,12 +14,12 @@ class Square:
             valueError: if not > 0
         """
 
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
     self.__size = size
 
     def area(self):
-        return self._size
+        return self.__size ** 2
