6fbfe39c557193467fa3733d8ced69d6622be7e0
emmaculate-exceel <emmaculate2exceel@live.com>
2023-12-22 02:26:32 -0800

    upper and last digit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def uppercase(str):
-    x = str
-    for x in range(len(x)):
-        print(x)
+    print(str.upper())
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number %= 10
-    print('{}'.format(number), end='\n')
-    return number
+    return number % 10
