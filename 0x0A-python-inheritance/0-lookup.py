404c57ccafc68e5f45f0cecf90ac4df0e482deec
emmaculate-exceel <emmaculate2exceel@live.com>
2024-01-08 09:15:09 -0800

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+    This module returns the list of available attributes
+    of an object
+"""
+
+
 def lookup(obj):
+    """ Returns a list of object """
+
     return(dir(obj))
