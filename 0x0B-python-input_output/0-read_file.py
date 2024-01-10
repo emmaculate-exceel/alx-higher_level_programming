133ade9863c0da4d827a5ca86695b3bc57e164fe
emmaculate-exceel <emmaculate2exceel@live.com>
2024-01-10 07:38:02 -0800

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """ using a function using the "with" keyword """
 
     with open(filename, encoding='utf-8') as f:
-        content = f.read()
-        print(content, end='')
+        x = f.read()
+        print(x, end="")
