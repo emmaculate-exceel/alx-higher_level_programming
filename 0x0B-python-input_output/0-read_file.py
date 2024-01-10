0c2ffa65f9ca7ed0393fef3bf0616c2772732a29
emmaculate-exceel <emmaculate2exceel@live.com>
2024-01-10 07:11:13 -0800

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ A function for reading of files """
 
+
 def read_file(filename=""):
     """ using a function using the "with" keyword """
-    
+
     with open(filename, 'r', encoding='utf-8') as f:
         content = f.read()
         print(content)
