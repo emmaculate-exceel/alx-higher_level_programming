f040a7a0467a75ac206cb8f73c3e68607f43e22d
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-14 03:59:38 +0100

    5tab

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- create a table that prints the full description of the table first_table
 -- you're not allowed to the DESCRIBE or EXPLAIN commands
 
-SELECT CONCAT ('SHOW CREATE TABLE', first_table , ';');
+SHOW CREATE TABLE first_table;
