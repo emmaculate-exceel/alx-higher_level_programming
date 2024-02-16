6866d4110f9c8b0bba90d958dd475cc3a5e36416
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-16 04:43:16 +0100

    update

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- top score in the DBMS
 -- of the student database
 
-SELECT name, score FROM second_table WHERE ORDER BY score DESC;
+SELECT name, score FROM second_table WHERE score ORDER BY score DESC;
