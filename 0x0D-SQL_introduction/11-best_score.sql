83ba64739def51f6efcaf1e8d57d759d92e4ca5b
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-16 05:28:12 +0100

    update1

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- best scores in DBMS
 -- for the second table
 
-SELECT name, score FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
