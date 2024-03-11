b9af0dca67ec3275e61aa0cf9ea06f6c8fb1d512
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-11 22:59:31 +0100

    8update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,13 @@
 #!/usr/bin/node
 
 const arg = process.argv[2];
-let n = parseInt(arg);
+const n = parseInt(arg);
 let i = 0;
-let a = 'X';
-if (isNaN(n)){
-    console.log('Missing size');
-}
-else{
-    while (i < n){
-	for (let a = 0; a < n; a++){
-		console.log(a);
-	    }
-	i++;
-    }
+if (isNaN(n)) {
+  console.log('Missing size');
+} else {
+  while (i < n) {
+    console.log('X'.repeat(n));
+    i++;
+  }
 }
