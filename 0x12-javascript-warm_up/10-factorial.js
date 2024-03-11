5d2387aa49c87667c053f47b9063df74c2e522a1
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-11 23:49:14 +0100

    10factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const x = parseInt(arg);
+function myfunc (n) {
+  if (isNaN(n)) {
+    return (1);
+  }
+  if (n < 0) {
+    return 0;
+  } else {
+    return (n + myfunc(n - 1));
+  }
+}
+console.log(myfunc(x));
