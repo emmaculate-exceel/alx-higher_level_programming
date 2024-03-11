76a456e0528a7c0f5c0854c3b6aba94a86937161
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-11 15:34:54 +0100

    2args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+
 const arg = process.argv.length;
-if (arg == 2){
-    console.log('No Argument');
-}
-else{
+if (arg > 2){
     console.log('Argument' + (arg > 3 ? 's': '') +' found');
 }
-//else{
-//    console.log('No argument')}
+else{
+    console.log('No argument');
+}
