ea9e24874c2b5d2012a80182c87073e33ad2503c
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-12 00:47:45 +0100

    update11

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,12 @@
 #!/urs/bin/python3
+""" A Base class for the programme"""
+
+
+class Base:
+    """ a private Attribute for the class """
+    
+    __nb_objects = 0
+    """ a method of the class """
+    def __init__(self, id=None):
+        if
+    
