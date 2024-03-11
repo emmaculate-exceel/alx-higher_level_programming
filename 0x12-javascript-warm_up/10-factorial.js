516f580e3c91c698562bce15856f67e9a6329738
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-12 00:18:40 +0100

    update10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
-const x = parseInt(arg);
-function myfunc (n) {
-  if (isNaN(n)) {
-    return (1);
-  }
-  if (n < 0) {
-    return 0;
-  } else {
-    return (n * myfunc(n - 1));
-  }
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
-console.log(myfunc(x));
+
+console.log(factorial(Number(process.argv[2])));
