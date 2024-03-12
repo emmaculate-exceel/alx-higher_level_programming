bf939a27d5291add03fadc0457140a9529f799f8
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-12 16:33:03 +0100

    3rect

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && !isNaN(w) && w > 0 && !isNaN(w)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+      Print () {
+	  for (let i = 0; i < this.h; i++) {
+	      console.log('X'.repeat(this.h);
+    }
+  }
+}
+
+module.exports = Rectangle;
