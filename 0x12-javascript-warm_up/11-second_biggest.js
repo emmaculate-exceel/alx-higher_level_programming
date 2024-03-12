bcaa2f3cbbd6d7f8f7daecf6e49e7ff8963fd4e0
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-12 01:07:16 +0100

    secondbiggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 
 const arg = process.argv;
-let x = arg.map(Number).slice(2,process.argv.length).sort((a, b) => a - b);
-if (x == 0){
-    return (1);
+let x = arg.map(Number)
+    .slice(2,process.argv.length)
+    .sort((a, b) => a - b);
+if (x <= 2){
+    console.log('0');
+}
+if (x === 3){
+    console.log('0');
 }
 else{
     console.log(x[x.length -2]);
