7cf1267d0bdc865dba522874ef2019187adf0361
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-13 04:10:40 +0100

    101sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 
 const dict = require('./101-data.js').dict;
-const new_dict = {};
+const new_dict = Object.getOwnPropertyNames(dict);
 
 for (let key in dict) {
-	new_dict[key] = dict[key];
+	if (new_dict[dict[key]] == undefined) {
+		new_dict[dict[key]] = [key];
+	}
+	else {
+		new_dict[dict[key]].push[];
 }
 
 console.log(new_dict);
