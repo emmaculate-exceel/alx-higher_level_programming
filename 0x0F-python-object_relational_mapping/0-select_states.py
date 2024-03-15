6be280d59755a20c3bc3c91fec91cc0481d210b9
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-15 19:05:38 +0100

    initial

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import MySQLdb
+"""
+class for MySQLdb
+"""
+def list_states(username, password, database):
+    """
+    function that creates a database
+    """ 
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+read me file for object relational mapping and more.
