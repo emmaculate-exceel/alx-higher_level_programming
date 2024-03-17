f9c27ef031f9f102304538ef3dbc6a82b26ab9ed
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-17 07:19:40 +0100

    select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,13 @@ from sys import argv
 class for MySQLdb
 """
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     def list_states(username, password, database):
         """
         function that creates a database
         """
+        
         db = MySQLdb.connect(host='localhost', port=3306,
                              user=username, passwd=password, db=database)
         cur = db.cursor()
