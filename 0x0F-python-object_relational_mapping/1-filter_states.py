88441788b7cf6bc251679d64ac22b6860a55598a
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-18 00:22:13 +0100

    1filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+list all states in the database
+which name starts with a capital N
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db_connect = MySQLdb.connect(host='localhost', port=3306, username=argv[1],
+                                 password=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
+    db_cursor.execute("SELECT * state WHERE REGEX '^[N]'")
+    db_fetch = db_cursor.fetchall
+
+    for row in db_fetch:
+        print(row)
