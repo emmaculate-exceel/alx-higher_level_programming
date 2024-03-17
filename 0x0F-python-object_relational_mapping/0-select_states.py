9ad0ac2a7482517ee8138ada6ed1308860469c68
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-17 22:07:07 +0100

    0select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,13 +12,20 @@ if __name__ == "__main__":
         """
         function that creates a database
         """
+
+        username = argv[1]
+        password = argv[2]
+        database = argv[3]
+        
         db = MySQLdb.connect(host='localhost', port=3306,
                              user=username, passwd=password, db=database)
         cur = db.cursor()
         cur.execute("SELECT * FROM states ORDER BY states.id")
         rows = cur.fetchall()
 
-        for row in rows:
-            print(row)
+        if(len(argv) < 4):
+            for row in rows:
+                print(row)
+                
         cur.close()
         db.close()
