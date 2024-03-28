4bbb1bbc74abe8905aa7cae33bf72a39767c7453
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-28 04:00:57 +0100

    updates

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #curl with post parameters
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
