e33ab3fab9501b034390d24a451e1d151a6ee61c
emmaculate-exceel <emmaculate2exceel@live.com>
2024-04-06 19:50:31 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import argparse
 
-    parser = argparse.ArgumentParser(description='Print the number of and list of arguments.')
+    parser = argparse.ArgumentParser(
+        description='Print the number of and list of arguments.')
 
-    parser.add_argument('arguments', nargs=argparse.REMAINDER, help='List of arguments')
+    parser.add_argument(
+        'arguments', nargs=argparse.REMAINDER, help='List of arguments')
 
     args = parser.parse_args()
 
     list_of_arguments = args.arguments
     number_of_arguments = len(list_of_arguments)
     list_of_arguments = args
-    
+
     print("Number of arguments:", number_of_arguments)
     print("List of arguments:", list_of_arguments)
-
