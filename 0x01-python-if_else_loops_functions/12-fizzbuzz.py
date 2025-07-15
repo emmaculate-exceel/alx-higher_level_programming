4362fac965ffbfe59d9dc919610c6ef9c728896d
emmaculate-exceel <emmaculate2exceel@live.com>
2025-07-15 13:58:26 +0100

    fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@
 
 
 def fizzbuzz():
-    for i in range(1,101):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=' ')
         elif i % 5 == 0:
@@ -12,6 +12,3 @@ def fizzbuzz():
             print("Fizz", end=' ')
         else:
             print(i, end=' ')
-
-
-fizzbuzz()
