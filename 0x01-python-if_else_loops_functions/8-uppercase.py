8f4d1c9fb45e97b9ec8252104ea379aeffe5ad22
emmaculate-exceel <emmaculate2exceel@live.com>
2025-07-15 20:23:55 +0100

    uppercase using chr() and ord() builtin python function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,4 @@ def uppercase(str):
         a = ord(i)
         if a >= 97 and a <= 122:
             a -= 32
-        print("{}".format(chr(a)), end="")
+        print("{} ".format(chr(a)), end="")
