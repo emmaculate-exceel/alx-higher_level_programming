6fc04a54376fbade091df69420d41c060e34c28e
emmaculate-exceel <emmaculate2exceel@live.com>
2025-07-16 19:43:13 +0100

    update

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number < 0:
+    if number <= 0:
         print(-(number) % 10, end='')
     else:
         print((number) % 10, end='')
     return abs(number)
-print_last_digit(98)
-print_last_digit(0)
-print_last_digit(-1024)
